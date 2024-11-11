ab47753a9391f642c5635b413704c78f684a5498
delll <delll@DESKTOP-FHD2Q16>
2024-11-11 13:27:15 +0545

    Add project files.

---
+++ project 1/Models/student.cs
@@ -0,0 +1,10 @@
+﻿namespace project_1.Models
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string Name{ get; set; }
+        public int Age{ get; set; }
+        public float Fee { get; set; }
+    }
+}
